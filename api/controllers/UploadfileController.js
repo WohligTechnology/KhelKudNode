54be51d22ef29fc22e92f8e66ea34339695fca7b
vigkast <vigwohlig@gmail.com>
2015-10-12 16:40:17 +0530

    err removed

--- api/controllers/UploadfileController.js
+++ api/controllers/UploadfileController.js
@@ -14,7 +14,9 @@ module.exports = {
                     });
                 });
                 source.on('error', function(err) {
-                    console.log(err);
+                    if (err) {
+                        console.log(err);
+                    }
                 });
             });
             return res.json({
