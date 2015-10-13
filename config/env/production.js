a4b6301a20f0bed15be04fa9b34dd58f3e9b5ae9
vigkast <vigwohlig@gmail.com>
2015-10-13 18:32:58 +0530

    81 port

--- config/env/production.js
+++ config/env/production.js
@@ -17,15 +17,15 @@ module.exports = {
    * environment (see config/connections.js and config/models.js )           *
    ***************************************************************************/
 
-  models: {
-    connection: 'someMongodbServer'
-  },
+  // models: {
+  //   connection: 'someMongodbServer'
+  // },
 
   /***************************************************************************
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  port: 1337,
+  port: 81,
 
   /***************************************************************************
    * Set the log level in production environment to "silent"                 *
