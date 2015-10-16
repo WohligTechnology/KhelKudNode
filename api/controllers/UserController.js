86fdad3e1990fbcd48608f302b6bd2878ed3fe75
vigkast <vigwohlig@gmail.com>
2015-10-16 17:15:30 +0530

    timeout increased

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -179,7 +179,7 @@ module.exports = {
                                                                 if (num < result.length) {
                                                                     setTimeout(function() {
                                                                         createuser(num);
-                                                                    }, 150);
+                                                                    }, 250);
                                                                 } else {
                                                                     res.json("Done");
                                                                 }
@@ -189,7 +189,7 @@ module.exports = {
                                                                 if (num < result.length) {
                                                                     setTimeout(function() {
                                                                         createuser(num);
-                                                                    }, 150);
+                                                                    }, 250);
                                                                 } else {
                                                                     res.json("Done");
                                                                 }
@@ -221,7 +221,7 @@ module.exports = {
                                                         if (num < result.length) {
                                                             setTimeout(function() {
                                                                 createuser(num);
-                                                            }, 150);
+                                                            }, 250);
                                                         } else {
                                                             res.json("Done");
                                                         }
@@ -231,7 +231,7 @@ module.exports = {
                                                         if (num < result.length) {
                                                             setTimeout(function() {
                                                                 createuser(num);
-                                                            }, 150);
+                                                            }, 250);
                                                         } else {
                                                             res.json("Done");
                                                         }
