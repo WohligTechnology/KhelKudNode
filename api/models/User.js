f3b762e41d34d0447c99f0113f3e3c754c76b153
vigkast <vigwohlig@gmail.com>
2015-10-20 11:54:27 +0530

    email check removed

--- api/models/User.js
+++ api/models/User.js
@@ -14,154 +14,135 @@ module.exports = {
             if (db) {
                 if (!data._id) {
                     data._id = sails.ObjectID();
-                    db.collection("user").find({
-                        email: data.email
+                    db.collection("team").find({
+                        "pincode.pincode": data.pincode
+                    }, {
+                        "_id": 1,
+                        "teamname": 1,
+                        "email": 1
                     }).toArray(function(err, data2) {
-                        if (err) {
+                        if (data2 && data2[0]) {
+                            data.team = data2[0];
+                            saveuser();
+                        } else if (err) {
                             console.log(err);
                             callback({
-                                value: false,
-                                comment: "Error"
-                            });
-                            db.close();
-                        } else if (data2 && data2[0]) {
-                            callback({
-                                value: false,
-                                comment: "User already exists"
+                                value: false
                             });
                             db.close();
                         } else {
-                            db.collection("team").find({
-                                "pincode.pincode": data.pincode
-                            }, {
-                                "_id": 1,
-                                "teamname": 1,
-                                "email": 1
-                            }).toArray(function(err, data2) {
-                                if (data2 && data2[0]) {
-                                    data.team = data2[0];
-                                    saveuser();
-                                } else if (err) {
+                            callback({
+                                value: false,
+                                comment: "No such pincode"
+                            });
+                            db.close();
+                        }
+                    });
+
+                    function saveuser() {
+                        db.collection('user').aggregate([{
+                            $group: {
+                                _id: null,
+                                regno: {
+                                    $max: "$regno"
+                                }
+                            }
+                        }, {
+                            $project: {
+                                _id: 0,
+                                regno: 1
+                            }
+                        }]).toArray(function(err, data2) {
+                            if (err) {
+                                console.log(err);
+                                callback({
+                                    value: false,
+                                    comment: "Error"
+                                });
+                                db.close();
+                            } else if (data2 && data2[0]) {
+                                data.regno = data2[0].regno + 1;
+                                callme();
+                            } else {
+                                data.regno = 9913;
+                                callme();
+                            }
+                        });
+
+                        function callme() {
+                            db.collection('user').insert(data, function(err, created) {
+                                if (err) {
                                     console.log(err);
                                     callback({
-                                        value: false
+                                        value: false,
+                                        comment: "Error"
                                     });
                                     db.close();
+                                } else if (created) {
+                                    data.regno = data.regno.toString();
+                                    var name = data.firstname + " " + data.middlename + " " + data.lastname;
+                                    if (data.regno.length == 4) {
+                                        var regnos = "R00" + data.regno;
+                                    } else if (data.regno.length == 5) {
+                                        var regnos = "R0" + data.regno;
+                                    } else {
+                                        var regnos = "R" + data.regno;
+                                    }
+                                    var template_name = "bherpo";
+                                    var template_content = [{
+                                        "name": "bherpo",
+                                        "content": "bherpo"
+                                    }]
+                                    var message = {
+                                        "from_email": sails.fromEmail,
+                                        "from_name": sails.fromName,
+                                        "to": [{
+                                            "email": data.email,
+                                            "type": "to"
+                                        }, {
+                                            "email": data.team.email,
+                                            "type": "cc"
+                                        }],
+                                        "global_merge_vars": [{
+                                            "name": "events",
+                                            "content": data.sportsdata
+                                        }, {
+                                            "name": "number",
+                                            "content": regnos
+                                        }, {
+                                            "name": "team",
+                                            "content": data.team.teamname
+                                        }, {
+                                            "name": "mob",
+                                            "content": data.mobileno
+                                        }, {
+                                            "name": "name",
+                                            "content": name
+                                        }]
+                                    };
+                                    sails.mandrill_client.messages.sendTemplate({
+                                        "template_name": template_name,
+                                        "template_content": template_content,
+                                        "message": message
+                                    }, function(result) {
+                                        callback({
+                                            value: true,
+                                            comment: "Mail Sent"
+                                        });
+                                        db.close();
+                                    }, function(e) {
+                                        console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+                                    });
                                 } else {
                                     callback({
                                         value: false,
-                                        comment: "No such pincode"
+                                        comment: "Not created"
                                     });
                                     db.close();
                                 }
                             });
-
-                            function saveuser() {
-                                db.collection('user').aggregate([{
-                                    $group: {
-                                        _id: null,
-                                        regno: {
-                                            $max: "$regno"
-                                        }
-                                    }
-                                }, {
-                                    $project: {
-                                        _id: 0,
-                                        regno: 1
-                                    }
-                                }]).toArray(function(err, data2) {
-                                    if (err) {
-                                        console.log(err);
-                                        callback({
-                                            value: false,
-                                            comment: "Error"
-                                        });
-                                        db.close();
-                                    } else if (data2 && data2[0]) {
-                                        data.regno = data2[0].regno + 1;
-                                        callme();
-                                    } else {
-                                        data.regno = 9913;
-                                        callme();
-                                    }
-                                });
-
-                                function callme() {
-                                    db.collection('user').insert(data, function(err, created) {
-                                        if (err) {
-                                            console.log(err);
-                                            callback({
-                                                value: false,
-                                                comment: "Error"
-                                            });
-                                            db.close();
-                                        } else if (created) {
-                                            data.regno = data.regno.toString();
-                                            var name = data.firstname + " " + data.middlename + " " + data.lastname;
-                                            if (data.regno.length == 4) {
-                                                var regnos = "R00" + data.regno;
-                                            } else if (data.regno.length == 5) {
-                                                var regnos = "R0" + data.regno;
-                                            } else {
-                                                var regnos = "R" + data.regno;
-                                            }
-                                            var template_name = "bherpo";
-                                            var template_content = [{
-                                                "name": "bherpo",
-                                                "content": "bherpo"
-                                            }]
-                                            var message = {
-                                                "from_email": sails.fromEmail,
-                                                "from_name": sails.fromName,
-                                                "to": [{
-                                                    "email": data.email,
-                                                    "type": "to"
-                                                }, {
-                                                    "email": data.team.email,
-                                                    "type": "cc"
-                                                }],
-                                                "global_merge_vars": [{
-                                                    "name": "events",
-                                                    "content": data.sportsdata
-                                                }, {
-                                                    "name": "number",
-                                                    "content": regnos
-                                                }, {
-                                                    "name": "team",
-                                                    "content": data.team.teamname
-                                                }, {
-                                                    "name": "mob",
-                                                    "content": data.mobileno
-                                                }, {
-                                                    "name": "name",
-                                                    "content": name
-                                                }]
-                                            };
-                                            sails.mandrill_client.messages.sendTemplate({
-                                                "template_name": template_name,
-                                                "template_content": template_content,
-                                                "message": message
-                                            }, function(result) {
-                                                callback({
-                                                    value: true,
-                                                    comment: "Mail Sent"
-                                                });
-                                                db.close();
-                                            }, function(e) {
-                                                console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
-                                            });
-                                        } else {
-                                            callback({
-                                                value: false,
-                                                comment: "Not created"
-                                            });
-                                            db.close();
-                                        }
-                                    });
-                                }
-                            }
                         }
-                    });
+                    }
                 } else {
                     var user = sails.ObjectID(data._id);
                     delete data._id;
