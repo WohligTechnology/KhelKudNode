27add76c51511e0e8e4d40ede3ab6447f29b7322
vigkast <vigwohlig@gmail.com>
2015-10-30 15:38:04 +0530

    schedule excel

--- api/controllers/ScheduleController.js
+++ api/controllers/ScheduleController.js
@@ -57,5 +57,86 @@ module.exports = {
             res.json(data);
         };
         Schedule.findlimited(req.body, callback);
+    },
+    excelobject: function(req, res) {
+        var zone = req.param("zone");
+        var venue = req.param("venue");
+        var report = req.param("report");
+        var date = req.param("date");
+        var team = req.param("team");
+        sails.query(function(err, db) {
+            if (err) {
+                console.log(err);
+            }
+            if (db) {
+                db.open(function(err, db) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    if (db) {
+                        res.connection.setTimeout(200000);
+                        req.connection.setTimeout(200000);
+                        req.file("file").upload(function(err, uploadedFiles) {
+                            if (err) {
+                                console.log(err);
+                            }
+                            _.each(uploadedFiles, function(n) {
+                                writedata = n.fd;
+                                excelcall(writedata);
+                            });
+                        });
+
+                        function excelcall(datapath) {
+                            var outputpath = "./.tmp/output.json";
+                            sails.xlsxj({
+                                input: datapath,
+                                output: outputpath
+                            }, function(err, result) {
+                                if (err) {
+                                    console.error(err);
+                                }
+                                if (result) {
+                                    sails.fs.unlink(datapath, function(data) {
+                                        if (data) {
+                                            sails.fs.unlink(outputpath, function(data2) {});
+                                        }
+                                    });
+                                    createsch();
+
+                                    function createsch() {
+                                        var m = {};
+                                        _.forIn(result[0], function(value, key) {
+                                            if (key === "category") {
+                                                m.cat = "Category";
+                                            } else if (key === "zone") {
+                                                m.cat = "Zone";
+                                            } else if (key === "ground") {
+                                                m.cat = "Ground";
+                                            }
+                                        });
+                                        _.forIn(result[0], function(value, key) {
+                                            if (key === "court") {
+                                                m.cou = "Court";
+                                            } else if (key === "track") {
+                                                m.cou = "Track";
+                                            }
+                                        });
+                                        m.zone = zone;
+                                        m.report = report;
+                                        m.team = team;
+                                        m.date = date;
+                                        m.venue = venue;
+                                        m.events = result;
+                                        Schedule.save(m, function(respo) {
+                                            res.json(respo);
+                                        });
+                                    }
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+        });
     }
 };
