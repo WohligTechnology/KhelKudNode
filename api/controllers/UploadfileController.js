d5a1eaae1c240979c58e592b008b9958ebcf127e
vigkast <vigwohlig@gmail.com>
2015-11-06 16:15:52 +0530

    dest.path

--- api/controllers/UploadfileController.js
+++ api/controllers/UploadfileController.js
@@ -10,8 +10,8 @@ module.exports = {
                     image.resize(800, 800, "lanczos", function(err, image) {
                         image.toBuffer('jpg', function(err, buffer) {
                             var dest = sails.fs.createWriteStream('./bherpoimg/' + n.fd);
-                            console.log(dest);
-                            sails.fs.writeFileSync(dest, buffer);
+                            console.log(dest.path);
+                            sails.fs.writeFileSync(dest.path, buffer);
                             sails.fs.unlink(oldpath, function(data) {});
                         });
                     });
