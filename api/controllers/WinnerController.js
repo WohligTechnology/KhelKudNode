0fceb71e88ee3a2a0b3b09ad41caf95771e73185
vigkast <vigwohlig@gmail.com>
2015-12-24 16:08:09 +0530

    winner added

---
+++ api/controllers/WinnerController.js
@@ -0,0 +1,61 @@
+module.exports = {
+    save: function(req, res) {
+        if (req.body._id) {
+            if (req.body._id != "" && sails.ObjectID.isValid(req.body._id)) {
+                winner();
+            } else {
+                res.json({
+                    value: "false",
+                    comment: "Winner-id is incorrect"
+                });
+            }
+        } else {
+            winner();
+        }
+
+        function winner() {
+            var print = function(data) {
+                res.json(data);
+            }
+            Winner.save(req.body, print);
+        }
+    },
+    delete: function(req, res) {
+        if (req.body._id && req.body._id != "" && sails.ObjectID.isValid(req.body._id)) {
+            var print = function(data) {
+                res.json(data);
+            }
+            Winner.delete(req.body, print);
+        } else {
+            res.json({
+                value: "false",
+                comment: "Winner-id is incorrect"
+            });
+        }
+    },
+    find: function(req, res) {
+        function callback(data) {
+            res.json(data);
+        };
+        Winner.find(req.body, callback);
+    },
+    findone: function(req, res) {
+        if (req.body._id && req.body._id != "" && sails.ObjectID.isValid(req.body._id)) {
+            var print = function(data) {
+                res.json(data);
+            }
+            Winner.findone(req.body, print);
+        } else {
+            res.json({
+                value: "false",
+                comment: "Winner-id is incorrect"
+            });
+        }
+    },
+    findlimited: function(req, res) {
+        function callback(data) {
+            res.json(data);
+        };
+        Winner.findlimited(req.body, callback);
+    }
+};
